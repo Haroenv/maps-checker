85e8aa202ed2d555a3f587ba5201330519b0f2ed
Haroen Viaene <hello@haroen.me>
2015-11-18 21:57:13 +0100

    notice galore

--- src/main.js
+++ src/main.js
@@ -6,4 +6,35 @@
 (function() {
 	//todo: remove in prod
 	console.log('Aight.');
+
+	/**
+	 * add a notice, removeable by clicking the 'close button'
+	 * Creates <p class="notice"><span class="notice--close"></span>text</p>
+	 * @param  {string} text the notice text
+	 */
+	var notice = function(text){
+		var notice = document.createElement('p');
+		var close = document.createElement('span');
+		var content = document.createTextNode(text);
+
+		notice.appendChild(close);
+		notice.appendChild(content);
+
+		close.className += 'notice--close';
+		notice.className += 'notice';
+
+		if ("webkitAppearance" in document.body.style) {
+			close.style.webkitAppearance = "searchfield-cancel-button";
+		} else {
+			close.appendChild(document.createTextNode('x'));
+		}
+
+		document.body.insertBefore(notice, document.body.firstChild);
+
+		close.addEventListener('click',function(){
+			document.body.removeChild(notice);
+		});
+	};
+
+	notice('This site uses cookies to function. By continuing to use this site you agree to save local cookies. ');
 })();
