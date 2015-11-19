e69034dd8ef0c3b14f7a1fcbafb886052eb07eff
Haroen Viaene <hello@haroen.me>
2015-11-19 22:43:30 +0100

    graph work

--- src/main.js
+++ src/main.js
@@ -65,31 +65,12 @@
 	calculate();
 
 	/**
-	 * logging your own estimate
-	 */
-	var log = function() {
-		var results = document.querySelector('.result');
-		var estimate = results.querySelector('.result--number');
-		var log = results.querySelector('.result--button');
-		log.addEventListener('click',function(){
-			var est = estimate.innerHTML.replace(/\s+/g, '').replace(/,/g,'.');
-			//todo: save to localstorage
-			var data = JSON.parse(window.localStorage.getItem('data')) || [];
-			data.push({time: Date.now(),value: est});
-			window.localStorage.setItem('data',JSON.stringify(data));
-			notice('logged ' + est);
-		});
-	}
-
-	log();
-
-	/**
 	 * get the values the logs stored in localStorage
 	 * @return {array} the values
 	 */
 	var getDataValues = function() {
 		var data = [];
-		JSON.parse(window.localStorage.getItem('data')).forEach(function(e,i){
+		(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
 			data.push(e.value);
 		});
 		return data;
@@ -101,18 +82,18 @@
 	 */
 	var getDataTimes = function() {
 		var data = [];
-		JSON.parse(window.localStorage.getItem('data')).forEach(function(e,i){
-			data.push(e.time.getDate() + '-' + (e.time.getMonth() + 1) + ' ' + e.time.getHours() + ':' + e.time.getMinutes());
+		(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
+			var date = new Date(e.time);
+			data.push(date.getDate() + '-' + (date.getMonth() + 1) + ' ' + date.getHours() + ':' + date.getMinutes());
 		});
 		return data;
 	}
 
 	/**
 	 * Show the graph
-	 * TODO: populate
 	 */
-	var graph = function() {
-		var now = new Date;
+	var graph;
+	var initGraph = function() {
 		var data = {
 				labels: getDataTimes(),
 				datasets: [{
@@ -140,8 +121,54 @@
 		};
 		var ctx = document.getElementById("myChart").getContext("2d");
 		Chart.defaults.global.responsive = true;
-		var myLineChart = new Chart(ctx).Line(data, options);
+		graph = new Chart(ctx).Line({
+				labels: getDataTimes(),
+				datasets: [{
+						label: "Your data",
+						fillColor: "#F44336",
+						strokeColor: "#F44336",
+						pointHighlightFill: "#F44336",
+						data: getDataValues()
+				}, {
+						label: "Google Maps",
+						fillColor: "rgba(0,0,0,0)",
+						strokeColor: "#000",
+						pointHighlightFill: "#000",
+					  data: [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+				}]
+		}, {
+				bezierCurveTension: 0.3,
+				pointDot: false,
+				datasetStroke: false,
+				datasetStrokeWidth: 0,
+				datasetFill: true,
+				tooltipTitleFontFamily: "-apple-system, system, sans-serif"
+
+		});
 	}
 
-	graph();
+	initGraph();
+
+	/**
+	 * logging your own estimate
+	 */
+	var log = function() {
+		var results = document.querySelector('.result');
+		var estimate = results.querySelector('.result--number');
+		var log = results.querySelector('.result--button');
+		log.addEventListener('click',function(){
+			var est = estimate.innerHTML.replace(/\s+/g, '').replace(/,/g,'.');
+			if (isNaN(est)) {
+				notice(est + ' is not a number');
+			} else {
+				var data = JSON.parse(window.localStorage.getItem('data')) || [];
+				data.push({time: Date.now(),value: est});
+				window.localStorage.setItem('data',JSON.stringify(data));
+			}
+
+		});
+		graph.update();
+	}
+
+	log();
 // })();
