a22aa92cdf4b533819b56ca7663ea019a5a7953a
Haroen Viaene <hello@haroen.me>
2015-11-29 11:00:33 +0100

    indentation && better guess

--- src/main.js
+++ src/main.js
@@ -18,31 +18,31 @@
 	 * @author Haroen Viaene <hello@haroen.me>
 	 * @license https://github.com/haroenv/notice CC-4.0-BY
 	 */
-	 var notice = function(text){
-	 	var notice = document.createElement('p');
-	 	var close = document.createElement('span');
-	 	var content = document.createTextNode(text);
+	var notice = function(text){
+		var notice = document.createElement('p');
+		var close = document.createElement('span');
+		var content = document.createTextNode(text);
 
-	 	notice.appendChild(close);
-	 	notice.appendChild(content);
+		notice.appendChild(close);
+		notice.appendChild(content);
 
-	 	close.className += 'notice--close';
-	 	notice.className += 'notice';
+		close.className += 'notice--close';
+		notice.className += 'notice';
 
-	 	close.appendChild(document.createTextNode('✕'));
+		close.appendChild(document.createTextNode('✕'));
 
-	 	document.body.insertBefore(notice, document.body.firstChild);
+		document.body.insertBefore(notice, document.body.firstChild);
 
-	 	close.addEventListener('click',function(){
-	 		document.body.removeChild(notice);
-	 	});
-	 };
+		close.addEventListener('click',function(){
+			document.body.removeChild(notice);
+		});
+	};
 
-	 var loadSearchFromStorage = function() {
-	 	document.getElementById('from').value = window.localStorage.getItem('from');
-	 	document.getElementById('to').value = window.localStorage.getItem('to');
-	 	document.getElementById('mode').value = window.localStorage.getItem('mode');
-	 }
+	var loadSearchFromStorage = function() {
+		document.getElementById('from').value = window.localStorage.getItem('from');
+		document.getElementById('to').value = window.localStorage.getItem('to');
+		document.getElementById('mode').value = window.localStorage.getItem('mode');
+	}
 
 	/**
 	 * Make the search results appear
@@ -50,34 +50,34 @@
 	 * display the travel time
 	 * todo: do the search
 	 */
-	 var calculate = function() {
-	 	var search = document.querySelector('.search');
-	 	var from = document.getElementById('from').value;
-	 	var to = document.getElementById('to').value;
-	 	var mode = document.getElementById('mode').value;
-	 	var submit = search.getElementsByTagName('button')[0];
+	var calculate = function() {
+		var search = document.querySelector('.search');
+		var from = document.getElementById('from').value;
+		var to = document.getElementById('to').value;
+		var mode = document.getElementById('mode').value;
+		var submit = search.getElementsByTagName('button')[0];
 
-	 	var directionsService = new google.maps.DirectionsService;
-	 	var directionsDisplay = new google.maps.DirectionsRenderer;
-	 	map = new google.maps.Map(document.getElementById('map'), {
-	 		zoom: 7,
-	 		center: {lat: 50.43, lng: 4.36}
-	 	});
-	 	directionsDisplay.setMap(map);
+		var directionsService = new google.maps.DirectionsService;
+		var directionsDisplay = new google.maps.DirectionsRenderer;
+		map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 7,
+			center: {lat: 50.43, lng: 4.36}
+		});
+		directionsDisplay.setMap(map);
 
-	  // var onChangeHandler = function() {
-	  //   calculateAndDisplayRoute(directionsService, directionsDisplay);
-	  // };
-	  // document.getElementById('submit').addEventListener('click', onChangeHandler)
+	 // var onChangeHandler = function() {
+	 //   calculateAndDisplayRoute(directionsService, directionsDisplay);
+	 // };
+	 // document.getElementById('submit').addEventListener('click', onChangeHandler)
 
-	  submit.addEventListener('click',function(){
-	  	document.querySelector('.result--number').innerHTML = googleTravelTime;
-	  	from = document.getElementById('from').value;
-	  	to = document.getElementById('to').value;
-	  	mode = document.getElementById('mode').value;
-	  	window.localStorage.setItem('from',from);
-	  	window.localStorage.setItem('to',to);
-	  	window.localStorage.setItem('mode',mode);
+	 submit.addEventListener('click',function(){
+	 	document.querySelector('.result--number').innerHTML = googleTravelTime;
+	 	from = document.getElementById('from').value;
+	 	to = document.getElementById('to').value;
+	 	mode = document.getElementById('mode').value;
+	 	window.localStorage.setItem('from',from);
+	 	window.localStorage.setItem('to',to);
+	 	window.localStorage.setItem('mode',mode);
 		//todo: search on google
 		var expected = calculateAndDisplayRoute(directionsService, directionsDisplay, from, to, mode);
 		console.log(expected);
@@ -92,91 +92,91 @@
 	 * get the values the logs stored in localStorage
 	 * @return {array} the values
 	 */
-	 var getDataValues = function() {
-	 	var data = [];
-	 	(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
-	 		data.push(e.value);
-	 	});
-	 	return data;
-	 }
+	var getDataValues = function() {
+		var data = [];
+		(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
+			data.push(e.value);
+		});
+		return data;
+	}
 
 	/**
 	 * get the values of Google maps the logs stored in localStorage
 	 * @return {array} the values
 	 */
-	 var getDataGoogleValues = function() {
-	 	var data = [];
-	 	(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
-	 		data.push(e.google);
-	 	});
-	 	return data;
-	 }
+	var getDataGoogleValues = function() {
+		var data = [];
+		(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
+			data.push(e.google);
+		});
+		return data;
+	}
 
 	/**
 	 * Get the times stored in localStorage
 	 * @return {array} the times a log occured
 	 */
-	 var getDataTimes = function() {
-	 	var data = [];
-	 	(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
-	 		var date = new Date(e.time);
-	 		data.push(date.getDate() + '-' + (date.getMonth() + 1) + ' ' + date.getHours() + ':' + date.getMinutes());
-	 	});
-	 	return data;
-	 }
+	var getDataTimes = function() {
+		var data = [];
+		(JSON.parse(window.localStorage.getItem('data')) || []).forEach(function(e,i){
+			var date = new Date(e.time);
+			data.push(date.getDate() + '-' + (date.getMonth() + 1) + ' ' + date.getHours() + ':' + date.getMinutes());
+		});
+		return data;
+	}
 
 	/**
 	 * Show the graph
 	 */
-	 var initGraph = function() {
-	 	graph ? graph.destroy() : null;
-	 	var ctx = document.getElementById("myChart").getContext("2d");
-	 	Chart.defaults.global.responsive = true;
-	 	graph = new Chart(ctx).Line({
-	 		labels: getDataTimes(),
-	 		datasets: [{
-	 			label: "Your data",
-	 			fillColor: "#F44336",
-	 			strokeColor: "#F44336",
-	 			pointHighlightFill: "#F44336",
-	 			data: getDataValues()
-	 		}, {
-	 			label: "Google Maps",
-	 			fillColor: "rgba(0,0,0,0)",
-	 			strokeColor: "#000",
-	 			pointHighlightFill: "#000",
-	 			data: getDataGoogleValues()
-	 		}]
-	 	}, {
-	 		bezierCurveTension: 0.3,
-	 		pointDot: false,
-	 		datasetStroke: false,
-	 		datasetStrokeWidth: 0,
-	 		datasetFill: true,
-	 		tooltipTitleFontFamily: "-apple-system, system, sans-serif"
+	var initGraph = function() {
+		graph ? graph.destroy() : null;
+		var ctx = document.getElementById("myChart").getContext("2d");
+		Chart.defaults.global.responsive = true;
+		graph = new Chart(ctx).Line({
+			labels: getDataTimes(),
+			datasets: [{
+				label: "Your data",
+				fillColor: "#F44336",
+				strokeColor: "#F44336",
+				pointHighlightFill: "#F44336",
+				data: getDataValues()
+			}, {
+				label: "Google Maps",
+				fillColor: "rgba(0,0,0,0)",
+				strokeColor: "#000",
+				pointHighlightFill: "#000",
+				data: getDataGoogleValues()
+			}]
+		}, {
+			bezierCurveTension: 0.3,
+			pointDot: false,
+			datasetStroke: false,
+			datasetStrokeWidth: 0,
+			datasetFill: true,
+			tooltipTitleFontFamily: "-apple-system, system, sans-serif"
 
-	 	});
-	 }
+		});
+	}
 
 	/**
 	 * logging your own estimate
 	 */
-	 var log = function() {
-	 	var results = document.querySelector('.result');
-	 	var estimate = results.querySelector('.result--number');
-	 	var log = results.querySelector('.result--button');
-	 	log.addEventListener('click',function(){
-	 		var est = estimate.innerHTML.replace(/\s+/g, '').replace(/,/g,'.');
-	 		if (isNaN(est)) {
-	 			notice(est + ' is not a number');
-	 		} else {
-	 			var data = JSON.parse(window.localStorage.getItem('data')) || [];
-	 			data.push({time: Date.now(),value: est,google: (googleTravelTime ? googleTravelTime : (data.length ? data[data.length-1].google : 10))});
-	 			window.localStorage.setItem('data',JSON.stringify(data));
-	 			initGraph();
-	 		}
-	 	});
-	 }
+	var log = function() {
+		var results = document.querySelector('.result');
+		var estimate = results.querySelector('.result--number');
+		var log = results.querySelector('.result--button');
+		log.addEventListener('click',function(){
+			var est = estimate.innerHTML.replace(/\s+/g, '').replace(/,/g,'.');
+			if (isNaN(est)) {
+				notice(est + ' is not a number');
+			} else {
+				var data = JSON.parse(window.localStorage.getItem('data')) || [];
+				data.push({time: Date.now(),value: est,google: (googleTravelTime ? googleTravelTime : (data.length ? data[data.length-1].google : 10))});
+				window.localStorage.setItem('data',JSON.stringify(data));
+				initGraph();
+			}
+		});
+	}
 
 
 	// function initMap() {
@@ -202,9 +202,8 @@
 		}, function(response, status) {
 			if (status === google.maps.DirectionsStatus.OK) {
 				directionsDisplay.setDirections(response);
-				// return response.routes[0].legs[0].duration.value;
-				console.log(response.routes[0]);
-				return "response"; //todo: figure out what's wrong.
+				console.log(response.routes[0].legs[0].duration.value);
+				return response.routes[0].legs[0].duration.value; //todo: figure out what's wrong.
 			} else {
 				notice('Directions request failed due to ' + status);
 			}
